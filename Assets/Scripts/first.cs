649f7c4392ac15c81c5b03c0cbfd0bb98e676ffa
Kush <kushwaharajan019@gmail.com>
2020-07-01 17:11:15 +0530

    MyfirstCommit
    Basic Hello

---
+++ Assets/Scripts/first.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class first : MonoBehaviour
+{
+    public Button button;
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+        
+    }
+
+   private void OnButtonClick()
+    {
+        Debug.Log("Button clicked");
+    }
+}
